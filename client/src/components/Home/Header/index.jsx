bc38f132b73ce66c6da8c4271d11c5a564cd5037
darko5r <28720396+darko5r@users.noreply.github.com>
2023-04-01 17:41:04 +0200

    Update index.jsx

--- client/src/components/Home/Header/index.jsx
+++ client/src/components/Home/Header/index.jsx
@@ -1,1 +1,17 @@
+import React from "react";
+import "./styles.css";
 
+
+const Header = () => (
+    <header className="home-header">
+      <h2>Login/Register template</h2>  
+      <h1>
+        <span>"</span> bronze-badger-blog <span>"</span>
+      </h1>
+      <p>
+        Added components without styling <br /> tryouts for DB connectivity.
+      </p>
+    </header>
+  );
+
+export default Header;

5f7436938117c68f347fe83837dfa4704667a55a
darko5r <28720396+darko5r@users.noreply.github.com>
2023-04-01 17:51:34 +0200

    Update index.jsx

--- client/src/pages/Home/index.jsx
+++ client/src/pages/Home/index.jsx
@@ -1,12 +1,20 @@
 import React from 'react';
+import Header from '../../components/Home/Header';
+import Login from '../../components/Home/Login/Login';
+import Register from '../../components/Home/Register/Register';
 
 const Home = () => {
   return (
     <div>
-        
+
       {/* Header */}
+      <Header />
 
       {/*Register/LogIn*/}
+      <div className="auth-container">
+        <Login />
+        <Register />
+      </div>
 
       {/* Search Bar */}
 

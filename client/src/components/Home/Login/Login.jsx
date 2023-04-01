2ceee1bec3c57d52f8d59414b0ef7f7b0e1d9a47
darko5r <28720396+darko5r@users.noreply.github.com>
2023-04-01 17:46:41 +0200

    Update Login.jsx

--- client/src/components/Home/Login/Login.jsx
+++ client/src/components/Home/Login/Login.jsx
@@ -1,1 +1,56 @@
+import React, { useState } from 'react';
 
+const Login = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+
+  const handleLogin = async (e) => {
+    e.preventDefault();
+    try {
+      const response = await fetch('/api/auth/login', {  // folders can change, depends what we choose 
+        method: 'POST',
+        headers: {
+          'Content-Type': 'application/json'
+        },
+        body: JSON.stringify({ email, password })
+      });
+      const data = await response.json();
+      console.log(data);
+      // successful login (needs implementation)
+    } catch (error) {
+      console.error(error);
+      // login error (needs implementation)
+    }
+  };
+
+  return (
+    <form onSubmit={handleLogin}>
+      <h2>Login</h2>
+      <label>
+        Email:
+        <input
+          type="email"
+          value={email}
+          onChange={(e) => setEmail(e.target.value)}
+        />
+      </label>
+      <label>
+        Password:
+        <div>
+          <input
+            type={showPassword ? 'text' : 'password'}
+            value={password}
+            onChange={(e) => setPassword(e.target.value)}
+          />
+          <button type="button" onClick={() => setShowPassword(!showPassword)}>
+            {showPassword ? 'Hide' : 'Show'}
+          </button>
+        </div>
+      </label>
+      <button type="submit">Login</button>
+    </form>
+  );
+};
+
+export default Login;

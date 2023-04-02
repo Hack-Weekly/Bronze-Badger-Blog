46bb484c2fcfb24bb6bd86286301d5b0c8b56ddd
darko5r <28720396+darko5r@users.noreply.github.com>
2023-04-02 11:27:20 +0200

    Update App.jsx

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -4,6 +4,7 @@ import Home from './pages/Home';
 import Posts from './pages/Posts';
 import Login from './components/Home/Login/Login';
 import Register from './components/Home/Register/Register';
+import SearchBar from './components/Home/SearchBar/search';
 import { Routes, Route } from 'react-router-dom';
 
 const App = () => {
@@ -14,6 +15,7 @@ const App = () => {
         <Route exact path='/posts/:id' element={<Posts />} />    
         <Route exact path='/login' element={<Login />} />
         <Route exact path='/register' element={<Register />} />
+        <Route exact path='/search' element={<SearchBar />} />
       </Routes>
     </div>
   );

87e17561313bdeeac09d795780b2463b8c1f9e9b
hobi9 <hans25@live.it>
2023-04-02 15:02:50 +0200

    cleaning up email and name validation

--- server/utils/validation.js
+++ server/utils/validation.js
@@ -1,16 +1,13 @@
-const isValidEmail = (email) => {
-  if (!email) return false;
-  return String(email)
+const isValidEmail = (email) =>
+  String(email)
     .toLowerCase()
     .match(
       /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|.(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/
     );
-};
 
 const isUserNameValid = (username) => {
-  if (!username || username.length <= 5) return false;
   const usernameRegex = /^[a-z0-9_.]+$/;
-  return usernameRegex.test(username) && username.length > 5;
+  return username && username.length > 5 && usernameRegex.test(username);
 };
 
 const isValidPassword = (password) => password && password.length > 5;

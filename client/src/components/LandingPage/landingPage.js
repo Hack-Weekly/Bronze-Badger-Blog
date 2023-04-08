f963d18158a635dc9aea4d25af3ddd3029e8d962
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-04-08 21:53:35 +0100

    create landingPage.js

---
+++ client/src/components/LandingPage/landingPage.js
@@ -0,0 +1,14 @@
+import React, { Component } from 'react'
+import '.../App.css'
+
+export default class Landing extends Component(){
+    redner(){
+        return(
+            <div className='landingPage-div'>
+                 <h1>Welcome to Bronze Badger Blog</h1>
+                 <h3></h3>
+                 <button></button>
+            </div>
+        )
+    }
+}

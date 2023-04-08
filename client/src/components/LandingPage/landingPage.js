6fd108f335de2c09508ee5c312ea74b76b0a9c88
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-04-09 00:24:24 +0100

    update on landing page

--- client/src/components/LandingPage/landingPage.js
+++ client/src/components/LandingPage/landingPage.js
@@ -5,9 +5,9 @@ export default class Landing extends Component(){
     redner(){
         return(
             <div className='landingPage-div'>
-                 <h1>Welcome to Bronze Badger Blog</h1>
-                 <h3></h3>
-                 <button></button>
+                 <h1 className='landing-h1'>Welcome to Bronze Badger Blog</h1>
+                 <h3 className='landing-h3'>A blog where users are able to post, read and interact with other blogs</h3>
+                 <button className='landing-btn'>Sign up</button>
             </div>
         )
     }

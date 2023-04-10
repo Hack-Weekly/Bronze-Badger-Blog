ad5cd1ad280710d2810dadbce4b6023d4f093a50
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-04-10 06:19:13 +0100

    break

--- client/src/components/LandingPage/LandingHome.jsx
+++ client/src/components/LandingPage/LandingHome.jsx
@@ -1,14 +1,13 @@
-import React, { Component } from 'react'
+import React from 'react'
+import "./landingStyle.css"
+
+const Landing  = () => {
+    <div className='landingPage-div'>
+        <h1 id='landing-h1'>Welcome to Bronze Badger Blog</h1>
+        <h3 id='landing-h3'>A blog where users are able to post, read and interact with other blog posts</h3>
+        <button id='landing-btn'>Sign up</button>
+    </div>
+}; 
 
 
-export default class Landing extends Component(){
-    redner(){
-        return(
-            <div className='landingPage-div'>
-                 <h1 id='landing-h1'>Welcome to Bronze Badger Blog</h1>
-                 <h3 id='landing-h3'>A blog where users are able to post, read and interact with other blog posts</h3>
-                 <button id='landing-btn'>Sign up</button>
-            </div>
-        )
-    }
-} 
+export default Landing;

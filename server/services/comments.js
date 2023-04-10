71a645fc79e380c3e66815dc17dd27e0699d8b8f
valentino <valentino.bestak@gmail.com>
2023-04-10 13:26:44 +0200

    comments init - create schema and service skeleton

---
+++ server/services/comments.js
@@ -0,0 +1,16 @@
+const prisma = require('../prisma/prisma');
+
+async function createComment() {}
+
+function deleteComment() {}
+
+function getComments() {}
+
+function getComment() {}
+
+module.exports = {
+  createComment,
+  deleteComment,
+  getComment,
+  getComments,
+};

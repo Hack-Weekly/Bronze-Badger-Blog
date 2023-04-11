21e4a9904e446e68a5a1a53cae06fe374c263e4e
Mohammed Usman <62765818+MohammedUsman-hub@users.noreply.github.com>
2023-04-11 05:56:12 +0100

    ]

--- client/src/pages/LandingPage/landingPage.jsx
+++ client/src/pages/LandingPage/landingPage.jsx
@@ -1,10 +1,12 @@
-
-/* 
+import React from "react";
+import LP from "../../components/LandingPage/LandingHome";
+/* */
 const LandingPage = () => {
     return(
         <div>
+            <p>Hello World</p>
             <LP/>
         </div>
     );
 };
-export default LandingPage;*/
+export default LandingPage;
